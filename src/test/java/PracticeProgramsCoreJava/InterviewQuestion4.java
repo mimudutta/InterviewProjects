0ae226ba8a7819b539712fbe1fef0306a18944e0
mimu <mimudutta@gmail.com>
2021-03-10 18:50:21 +0700

    adding new pgms and solutions

--- src/test/java/PracticeProgramsCoreJava/InterviewQuestion4.java
+++ src/test/java/PracticeProgramsCoreJava/InterviewQuestion4.java
@@ -5,27 +5,57 @@ import java.util.Scanner;
 public class InterviewQuestion4 {
 
 	public static void main(String[] args) {
+		
+		
+		String str1="QA Test Automation";  //op:no itam otuAtseTAQ
+		String revString="";
+		String finalString="";
+		
+		for(int i=str1.length()-1;i>=0;i--) {   //Step1:reverse the value string
+			revString=revString+str1.charAt(i);
+		}
+		System.out.println(revString);   
+		String tempString=revString.replace(" ", "");  //Step2:replace the spaces or special ch. in rev string
+		System.out.println(tempString);  
+		int idx1=str1.indexOf(" ");
+		int idx2=str1.indexOf(" ",str1.indexOf(" ")+1);
+		System.out.println("indexes are: "+idx1+","+idx2);//Step3:get the indexes of the spaces or special ch.
+		
+		for(int i=0;i<tempString.length();i++) {          //Step4:in another empty string add spaces or special ch. 
+			if(i==idx1||i==idx2-1) {
+				finalString=finalString+" ";
+			}
+			finalString=finalString+tempString.charAt(i);
+		}
+		System.out.println("Final O/p: "+finalString);
+		System.out.println("============================================================");
 
 		// I/P : @abxy@rt
 		// O/P : @tryx@ba
 
-		//Scanner scn = new Scanner(System.in);
-		// System.out.println("Enter the string:");
-		String str = "@abxy@rt"; // scn.next();
-		String opString = "";
+		String str2 = "@abxy@rt"; 
+		String revstr2 = "";
+		String opStr="";
 
 		
-		  for(int i=str.length()-1;i>=0;i--) { 	  
-			  opString=opString+str.charAt(i);
+		  for(int i=str2.length()-1;i>=0;i--) { 	  
+			  revstr2=revstr2+str2.charAt(i);
 		  }
-		System.out.println(opString); //tr@yxba@
+		System.out.println(revstr2); //tr@yxba@
+		String tempStr=revstr2.replace("@", "");
 		
-		for(int i=0;i<str.length();i++) {
-			if(str.charAt(i)==('@')) {
-				System.out.println(str.indexOf(str.charAt(i)));
+		int indx1=str2.indexOf("@");
+		int indx2=str2.indexOf("@", str2.indexOf("@")+1);
+		System.out.println("indexes are: "+indx1+","+indx2);
+		
+		for(int i=0;i<tempStr.length();i++) {
+			
+			if(i==indx1||i==indx2-1) {
+				opStr=opStr+"@";
 			}
+			opStr=opStr+tempStr.charAt(i);
 		}
-		
+		System.out.println("O/P:"+opStr);
 		
 
 	}
---
+++ src/test/java/PracticeProgramsCoreJava/interview10.java
@@ -0,0 +1,38 @@
+package PracticeProgramsCoreJava;
+
+import java.util.Arrays;
+
+public class interview10 {
+	
+	//Pgm to find average and then find nearest number to the average from an array
+
+	public static void main(String[] args) {
+		int[] ip= {10,13,12,18,15,22,16}; //average
+		int sum=0;
+		int avg;
+		Arrays.sort(ip);
+
+		for(int i=0; i<ip.length; i++) {
+			//System.out.println(ip[i]);
+			sum=sum+ip[i];
+		}
+		avg=sum/ip.length;
+		System.out.println("Average:"+avg);
+		
+		for(int i=ip.length-1;i>0; i--) {
+			if(ip[i]<15) {
+				System.out.println("Lowest no. nearest to the Average is:"+ip[i]);
+				break;
+			}	
+		
+		}
+		for(int i=0;i<ip.length; i++) {
+			if(ip[i]>15) {
+				System.out.println("Higher no. nearest to the Average is:"+ip[i]);
+				break;
+			}	
+		
+		}
+	}
+
+}
