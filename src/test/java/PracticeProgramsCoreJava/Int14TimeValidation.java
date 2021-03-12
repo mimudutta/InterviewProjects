6782543b1dde95110798679ec53f041dfbc2e38a
mimu <mimudutta@gmail.com>
2021-03-12 23:11:53 +0700

    adding solutions

---
+++ src/test/java/PracticeProgramsCoreJava/Int14TimeValidation.java
@@ -0,0 +1,47 @@
+package PracticeProgramsCoreJava;
+
+public class Int14TimeValidation {
+
+	public static void main(String[] args) {
+		
+
+		//validTime("25:59");
+		System.out.println(validTime("01:59"));
+	}
+	
+	public static boolean validTime(String time) {
+		
+		String[] val= time.split(":");
+		boolean flag1=true;
+		boolean flag2=true;
+		
+	
+		if((Integer.parseInt(val[0])>=0)&&(Integer.parseInt(val[0])<=24)) {
+			flag1=true;
+			System.out.println("valid");
+		}
+		else {
+			flag1=false;
+			System.out.println("Invalid");
+		}
+		
+		if((Integer.parseInt(val[1])>=0)&&(Integer.parseInt(val[1])<=59)) {
+			flag2=true;
+			System.out.println("valid");
+		}
+		else {
+			System.out.println("Invalid");
+			flag2=false;
+		}
+		System.out.println(flag1+"    "+flag2);
+		if(flag1&&flag2==true) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
+
+	}
+
+}
--- src/test/java/PracticeProgramsCoreJava/interview8.java
+++ src/test/java/PracticeProgramsCoreJava/interview8.java
@@ -2,7 +2,6 @@ package PracticeProgramsCoreJava;
 
 import java.util.Scanner;
 
-import org.omg.CORBA.PUBLIC_MEMBER;
 
 public class interview8 {
 	
@@ -11,27 +10,79 @@ public class interview8 {
 	//OP: nothing--not proper ip.
 	
 	public static void main(String args[]) {
-		//Scanner scnScanner=new Scanner(System.in);
+		Scanner scnScanner=new Scanner(System.in);
+		String address =null;
+		try {
+			address = scnScanner.next();
+		} catch (NumberFormatException e) {
+			System.out.println("Please Enter number in ipv4 address format only!"+ e);
+			//e.printStackTrace();
+		}
+		  
+		//System.out.println(ValidationChain("171.21.34.200"));
 		
-		String str="171.21.34.200";       
-		String[] Array=str.split(".");
-		
-		for(int i=0;i<Array.length-1;i++) {
-			//System.out.println(Array[i]);
-			if((Array[i]>0)&&(Array[i]<255)) {
-				
-			}
-			if else {
-				//qsdqsdqsd
-			}
-			if el
-			
+		if(ValidationChain(address)==true) {
+			System.out.println("valid IP");
+		}
+		else {
+			System.out.println("Invalid IP! Please Enter number in ipv4 address format only");
 		}
 		
 	}
-	
-	
-	
-	
+	public static boolean ValidationChain(String s) {
+		String str=s;       
+		boolean f1;
+		boolean f2;
+		boolean f3;
+		boolean f4;
+		String[] Array=str.split("\\.");
+		
+		/*
+		 * for(int i=0;i<Array.length;i++) {
+		 * if((Integer.parseInt(Array[i])<=255)&&(Integer.parseInt(Array[i])>=0)) {
+		 * f1=true; }else { f1=false; } }
+		 */
+		
+		if((Integer.parseInt(Array[0])<=255)&&(Integer.parseInt(Array[0])>=0)) {
+			  f1=true; 
+			}else { 
+				f1=false; 
+			} 
+		
+		if((Integer.parseInt(Array[1])<=255)&&(Integer.parseInt(Array[1])>=0)) {
+			  f2=true; 
+			}else { 
+				f2=false; 
+			} 
+		
+		if((Integer.parseInt(Array[2])<=255)&&(Integer.parseInt(Array[2])>=0)) {
+			  f3=true; 
+			}else { 
+				f3=false; 
+			} 
+		
+		if((Integer.parseInt(Array[3])<=255)&&(Integer.parseInt(Array[3])>=0)) {
+			  f4=true; 
+			}else { 
+				f4=false; 
+			} 
+		
+		System.out.println(f1+" "+f2+" "+f3+" "+f4);
+		if(f1&&f2&&f3&&f4==true) {
+			return true;
+		}
+		else {
+			return false;
+		}	
+		
+	}	
 
 }
+
+//10.0.0.40-----valid
+//171.21.34.256-----invalid
+//171.21.256.255-----invalid
+//171.256.21.255-----invalid
+//256.255.21.255------invalid
+//abcd-------invalid
+
